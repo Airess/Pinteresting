15c10b6edbc9488769b95e882f60dd60712eea03
Airess <beautebargains@gmail.com>
2014-06-02 20:50:27 -0400

    Created home and about pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+  def index
+  @notice = "This came from the pages controller"
+end
+
+  def about
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
-Rails.application.routes.draw do
+Pinteresting::Application.routes.draw do
+    root "pages#home"
+    
+    get "about", :to => 'pages#about'
+    get "contact", :to=> 'pages#contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
